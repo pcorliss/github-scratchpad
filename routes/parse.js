06bf8593d351e8751b11ee14238e924157f181fc
Philip Corliss <pcorliss@groupon.com>
2013-04-30 18:26:41 -0500

    Init Markdown and setup the parse action

--- routes/parse.js
+++ routes/parse.js
@@ -1,9 +1,32 @@
+var marked = require('marked');
+// Initialize marked
+marked.setOptions({
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  langPrefix: 'language-',
+  highlight: function(code, lang) {
+    if (lang === 'js') {
+      return highlighter.javascript(code);
+    }
+    return code;
+  }
+});
+
+console.log(marked('i am using __markdown__.'));
 
 /*
- * GET users listing.
+ * POST /parse
  */
 
 exports.parse = function(req, res){
   console.log("Params:", req.body);
-  res.send("respond with a resource");
+  markdown = req.body.markdown;
+  html = marked(markdown);
+  res.send({
+    html: html
+  });
 };
