c161882b9c9e872141f8871709db88a1a108e1cc
Philip Corliss <pcorliss@groupon.com>
2013-04-30 19:07:10 -0500

    Working live edit

---
+++ public/javascripts/parse.js
@@ -0,0 +1,28 @@
+console.log("Loaded parse.js");
+
+$(function() {
+  console.log("Dom Ready");
+
+  htmlTarget = $('#htmlTarget');
+
+  markdownChanged = function() {
+    markdownToConvert = $(this).val();
+    console.log("Change Event", markdownToConvert);
+    $.ajax({
+      type: "POST",
+      url: "/parse",
+      data: {
+        markdown: markdownToConvert
+      },
+      success: function(data){
+        console.log("Success:", data);
+        htmlTarget.html(data.html)
+      },
+      error: function(data, textStatus){
+        console.log("Error:", data, textStatus);
+      },
+    });
+  };
+
+  $('#markdownSource').bind('keypress', markdownChanged)
+});
