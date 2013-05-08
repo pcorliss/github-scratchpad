eea1ca93eecf2eaf7b2a23c0ebb4760a620059f1
Philip Corliss <pcorliss@groupon.com>
2013-05-08 12:45:11 -0500

    Removing debugging statements and comments.

--- public/javascripts/parse.js
+++ public/javascripts/parse.js
@@ -1,13 +1,8 @@
-console.log("Loaded parse.js");
-
 $(function() {
-  console.log("Dom Ready");
-
   htmlTarget = $('#htmlTarget');
 
   markdownChanged = function() {
     markdownToConvert = $('#markdownSource').val();
-    console.log("Change Event", markdownToConvert);
     $.ajax({
       type: "POST",
       url: "/parse",
@@ -15,7 +10,6 @@ $(function() {
         markdown: markdownToConvert
       },
       success: function(data){
-        console.log("Success:", data);
         htmlTarget.html(data.html)
       },
       error: function(data, textStatus){
--- routes/parse.js
+++ routes/parse.js
@@ -1,21 +1,9 @@
-//var marked = require('marked');
-
-//console.log(marked('i am using __markdown__.'));
-
-/*
- * POST /parse
- */
-
-
-
 exports.parse = function(marked) {
   return function(req, res){
-    console.log("Params:", req.body);
     var markdown = req.body.markdown;
     var html = marked(markdown);
     res.send({
       html: html
     });
   };
-
 };
